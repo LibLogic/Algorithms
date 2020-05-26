0cd92d99b6704582a4d3895ffdaf83b3f3fdefe3
Tom Hodgkinson <hdgknsn@gmail.com>
2020-05-25 21:02:37 -0400

    Bubble Sort in Javascript

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        [arr[j], arr[i]] = [arr[i], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 2, 9, 3, 7, 56, 1, -22, 300]));
